22169926a02143340db785e70b5e7f18535e3cda
nevanftrc <143558001+nevanftrc@users.noreply.github.com>
2025-04-03 09:35:21 +0200

    Add files via upload

---
+++ C#/RoomRevAPI/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  devServer: {
+    static: "./public",
+    historyApiFallback: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
